3beb71ea08b635b96e25ceae96429c2d17908d7a
Lirian Su <liriansu@gmail.com>
2017-08-04 18:58:44 +0800

    feat: update setup script

--- setup.sh
+++ setup.sh
@@ -1,34 +1,78 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# a setup script to initialize the dev-environment for lirian
+
 set -e
 
-test -d ~/.myconf || git clone --recurse-submodules https://github.com/LKI/myconf.git ~/.myconf
-
-DIR="`echo ~`/.myconf"
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-function bak {
-  if [ -e $1 ]
-  then
-    mv $1 $1.bak
-  fi
+usage () {
+    echo "usage: ${0} <command>"
+    echo "command:"
+    echo "         install:   install the configuration"
+    echo "         uninstall: uninstall the configuration"
+    exit 1
 }
 
-# Install dotvim
-bak .vimrc
-bak .vim
-bak .ideavimrc
-ln -sf ${DIR}/dotvim/vimrc ~/.vimrc
-ln -sf ${DIR}/dotvim ~/.vim
-ln -sf ${DIR}/.ideavimrc ~/.ideavimrc
+home="`echo ~`"
+confDir="${home}/.myconf"
 
-# Install .emacs.d
-# bak .emacs.d
-# ln -s ${DIR}/emacs.d ~/.emacs.d
+install () {
+    if [[ -z "`command -v git`" ]];
+    then
+        echo "please first install git"
+    fi
 
-# Install configs
-ln -sf ${DIR}/.gitconfig ~/.gitconfig
-ln -sf ${DIR}/.tmux.conf ~/.tmux.conf
-ln -sf ${DIR}/.zshrc ~/.zshrc
+    if [[ -d ${confDir} ]];
+    then
+        echo "${confDir} already exists. please uninstall first"
+    fi
 
-echo "Setup success."
+    git clone --recurse-submodules https://github.com/LKI/myconf.git ${confDir}
+
+    # install oh-my-zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+    # make soft links
+    ln -sf ${confDir}/.gitconfig   ${home}/.gitconfig
+    ln -sf ${confDir}/.ideavimrc   ${home}/.ideavimrc
+    ln -sf ${confDir}/.profile     ${home}/.profile
+    ln -sf ${confDir}/.tmux.conf   ${home}/.tmux.conf
+    ln -sf ${confDir}/dotvim       ${home}/.vim
+    ln -sf ${confDir}/dotvim/vimrc ${home}/.vimrc
+
+    echo 'source ~/.profile' >> ${home}/.zshrc
+}
+
+uninstall () {
+    rm ${home}/.gitconfig || true
+    rm ${home}/.ideavimrc || true
+    rm ${home}/.profile || true
+    rm ${home}/.tmux.conf || true
+    rm ${home}/.vim || true
+    rm ${home}/.vimrc || true
+
+    sed -i '/^source ~\/.profile/d' ${home}/.zshrc
+
+    rm -rf ${confDir}
+}
+
+if [[ ${#} -lt 1 ]];
+then
+    usage
+fi
+
+if [[ "${1}" == "install" ]];
+then
+    echo "installing"
+    install
+    echo "done"
+    exit 0
+fi
+
+if [[ "${1}" == "uninstall" ]];
+then
+    echo "uninstalling"
+    uninstall
+    echo "done"
+    exit 0
+fi
+
+usage
