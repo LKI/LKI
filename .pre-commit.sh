02bb76afc46c84f593f2ec6e3b091a4f1dc425f3
Lirian Su <liriansu@gmail.com>
2019-06-03 20:47:53 +0800

    feat: add pre-commit script

---
+++ .pre-commit.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# this pre-commit hook will run several checkings
+# go for a coffee if it's committing
+#
+# Install (*nix):
+#   ln -sf ~/.pre-commit.sh .git/hooks/pre-commit
+# Install (Windows):
+#   mklink .git\hooks\pre-commit C:\Users\liriansu\.pre-commit.sh
+set -e
+
+# /dev/null not work on Windows
+VENV="`pipenv --venv 2>&1`"
+
+if [[ ${VENV} =~ "Aborted" ]];
+then
+  exit 0;
+fi
+
+# get changed (modified + staged) python files
+PYTHON_FILES="`git diff --name-only --diff-filter=AM HEAD | grep --color=never '.py$' || true`"
+
+# do nothing if no python file
+if [[ ! "${PYTHON_FILES}" ]];
+then
+  exit 0
+fi
+
+if [[ ! -z "`pipenv run pip list | grep '^yapf '`" ]];
+then
+  echo 'Running yapf...'
+  pipenv run yapf -i -r ${PYTHON_FILES}
+  git add ${PYTHON_FILES}
+fi
+
+if [[ ! -z "`pipenv run pip list | grep '^black '`" ]];
+then
+  echo 'Running black...'
+  pipenv run black ${PYTHON_FILES}
+  git add ${PYTHON_FILES}
+fi
+
+if [[ ! -z "`pipenv run pip list | grep '^flake8 '`" ]];
+then
+  echo 'Running flake8...'
+  pipenv run flake8 ${PYTHON_FILES}
+fi
+
+PYLINT_FILES="`git diff --name-only --diff-filter=AM HEAD | grep --color=never '.py$' | grep -v migrations || true`"
+
+# do nothing if no pylint file
+if [ ! "${PYLINT_FILES}" ]; then
+  exit 0
+fi
+
+if [[ ! -z "`pipenv run pip list | grep '^pylint '`" ]];
+then
+  echo 'Running pylint...'
+  pipenv run pylint ${PYTHON_FILES}
+fi
+
+exit 0
