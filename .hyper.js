41dc00be1697894ff8c29638fd259caae5cd769d
Lirian Su <liriansu@gmail.com>
2019-12-05 19:30:10 +0800

    refactor: remove plugin, update vim

--- .hyper.js
+++ .hyper.js
@@ -149,8 +149,6 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    'hyper-akari',
-    'hyper-font-ligatures',
   ],
 
   // in development, you can create a directory under
