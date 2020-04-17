d9932cec8bddc7f532c3cae6d510c60d48f0c579
Lirian Su <liriansu@gmail.com>
2020-04-17 17:27:55 +0800

    feat: may switch to wsl

--- .hyper.js
+++ .hyper.js
@@ -103,10 +103,12 @@ module.exports = {
     //
     // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
+    // shell: 'C:\\Windows\\System32\\bash.exe',
     shell: 'D:\\CodeEnv\\Git\\git-cmd.exe',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
+    // shellArgs: ['--login'],
     shellArgs: ['--command=usr/bin/bash.exe', '-l', '-i'],
 
     // for environment variables
