77db5af886aa63dc317ff056174217b765cc1ab6
Lirian Su <liriansu@gmail.com>
2022-06-02 08:18:58 +0800

    fix(lki): format file

--- lki/fires.py
+++ lki/fires.py
@@ -60,7 +60,10 @@ class LKI(Command):
             _link(".profile")
             _link(".tmux.conf")
             if is_windows:
-                _link(".windows-terminal.json", "~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json")
+                _link(
+                    ".windows-terminal.json",
+                    "~/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json",
+                )
             # TODO: implement `lki install --vim`
             # _link("dotvim/vimrc", ".vimrc")
             #
